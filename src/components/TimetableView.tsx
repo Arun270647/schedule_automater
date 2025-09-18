849a7ee2d6b230c4f9add423a7d1725849d943de
Arun270647 <shankar72ind@gmail.com>
2025-09-18 18:11:20 +0530

    fixed bugs

--- src/components/TimetableView.tsx
+++ src/components/TimetableView.tsx
@@ -1,4 +1,4 @@
-import React, 'useState', useMemo } from 'react';
+import { React,useState, useMemo } from 'react';
 import { Download, Filter, User, BookOpen } from 'lucide-react';
 import { useData } from '../context/DataContext';
 
